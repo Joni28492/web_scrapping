378767313144835a9fefb2683ef7b6c0c633e761
Joni Fernandez <joni28492@gmail.com>
2023-10-26 17:45:19 +0200

    tripadvisor multinivel doble

---
+++ cruzverde_enlaces.py
@@ -0,0 +1,14 @@
+from scrapy.item import Field
+from scrapy.item import Item
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.selector import Selector
+from scrapy.loader.processors import MapCompose
+from scrapy.linkextractors import LinkExtractor
+from scrapy.loader import ItemLoader
+
+
+"""
+extraccion de urls de lo que no son etiquetas a
+https://www.cruzverde.cl/medicamentos/
+
+"""
